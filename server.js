3eb2a49ea9533b00d782638bf7a86fbb16b32b4a
James Vaughan <fdbk@riseup.net>
2020-05-10 20:39:25 -0400

    [jv] Add server routes for create, update, destroy, get FoodItem.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require('./models')
 var path = require('path')
 
 app.use(express.static('public'))
+app.use(express.json())
 
 app.get('/', async function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
@@ -13,6 +14,42 @@ app.get('/', async function(req, res) {
 app.get('/foodItems', async function(req, res) {
   const foodItems = await db.FoodItem.findAll()
   return res.send(foodItems)
-});
+})
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.post('/foodItems', async function(req, res) {
+  const { name, category, notes, status } = req.body
+  const foodItem = await db.FoodItem.create({ name, category, notes, status })
+
+  res.send(foodItem)
+})
+
+app.get('/foodItems/:id', async function (req, res) {
+  const id = req.params.id
+  const { name, category } = req.body
+  const foodItem = await db.FoodItem.findOne({ where: { id } })
+
+  res.send(foodItem)
+})
+
+app.put('/foodItems/:id', async function (req, res) {
+  const id = req.params.id
+  const { name, category, status, notes } = req.body
+  const foodItem = await db.FoodItem.findOne({ where: { id } })
+  if (name) foodItem.name = name
+  if (category) foodItem.category = category
+  if (status) foodItem.status = status
+  if (notes) foodItem.notes = notes
+  await foodItem.save()
+
+  res.sendStatus(200)
+})
+
+app.delete('/foodItems/:id', async function (req, res) {
+  const id = req.params.id
+  const foodItem = await db.FoodItem.findOne({ where: { id } })
+  await foodItem.destroy()
+
+  res.sendStatus(200)
+})
+
+app.listen(port, () => console.log(`Grocery List app listening at http://localhost:${port}`))
