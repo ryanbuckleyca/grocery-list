dea37e7ba8345c4ae6bcdffbd5565d374312dd83
Ryan Buckley <ryanbuckley@gmail.com>
2020-06-01 12:15:11 -0400

    Add trim() to user input to fix bug

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ const printToServer = msg => {
 }
 
 const titleCase = (str) => {
-  return str.toLowerCase().split(' ').map(function(word) {
+  return str.trim().toLowerCase().split(' ').map(function(word) {
     return word.replace(word[0], word[0].toUpperCase())
   }).join(' ')
 }
